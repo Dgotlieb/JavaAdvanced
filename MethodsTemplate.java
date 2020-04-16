fd97508e659e02c52c5bba511aea4a8bc110c13e
dgotlieb <Dgotlieb8@gmail.com>
2020-04-16 21:01:23 +0300

    Create MethodsTemplate.java

---
+++ MethodsTemplate.java
@@ -0,0 +1,9 @@
+public class MethodsTemplate {
+    public void PrintAll(String word){
+        System.out.println(word);
+    }
+    
+    private void privatelyPrintAll(String word){
+        System.out.println(word);
+    }
+}
