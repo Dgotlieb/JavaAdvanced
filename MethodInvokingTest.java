497d8cac944a0fcea5a8562b43f16e9db6b21840
dgotlieb <Dgotlieb8@gmail.com>
2020-04-16 21:00:51 +0300

    Create MethodInvokingTest.java

---
+++ MethodInvokingTest.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MethodInvokingTest {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        MethodsTemplate privateObject = new MethodsTemplate();
+        Method privateStringMethod = MethodsTemplate.class.getDeclaredMethod("privatelyPrintAll", String.class);
+        privateStringMethod.setAccessible(true);
+        privateStringMethod.invoke(privateObject, "private hello");
+
+        Method publicStringMethod = MethodsTemplate.class.getDeclaredMethod("PrintAll", String.class);
+        publicStringMethod.invoke(privateObject, "public hello");
+    }
+}
