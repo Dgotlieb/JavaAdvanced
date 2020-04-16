dfcec285e25beef3e902d36140004e90407b3fd1
dgotlieb <Dgotlieb8@gmail.com>
2020-04-16 20:52:34 +0300

    Create GenericsTest.java

---
+++ GenericsTest.java
@@ -0,0 +1,14 @@
+public class GenericsTest {
+    public static void main(String[] args) {
+        Integer[] wholeNums = {5,6};
+        String[] words = {"hello","World"};
+        printArray(wholeNums);
+        printArray(words);
+    }
+
+    public static <E> void printArray(E[] elements) {
+        for (E element : elements){
+            System.out.println(element);
+        }
+    }
+}
