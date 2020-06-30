531e65815981def3b066f1ad922228c46d9b4e56
dgotlieb <Dgotlieb8@gmail.com>
2020-06-30 19:48:02 +0300

    Update and rename JunitDemonstration.java to TestNGDemonstration.java

--- TestNGDemonstration.java
+++ TestNGDemonstration.java
@@ -1,10 +1,10 @@
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class JunitDemonstration {
+public class TestNGDemonstration {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         Method[] methods = PrintTest.class.getDeclaredMethods();
         for(Method method : methods){
